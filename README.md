31b0569f6da5a69a20ba80c3b4676750c9b51331
yuval4 <didi19289@gmail.com>
2022-11-25 10:17:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,132 @@
+# Wordle Global
+
+[wordle.global](https://wordle.global/)
+
+Open Source Wordle in a bunch of languages
+
+PULL REQUESTS WELCOME!
+
+It would be mega awesome if you could help in any way (especially with language addition/curation).
+
+**How to add a new language:**
+1. Make a folder in webapp/data/languages/ with the language code (e.g. en, de, fr, qya, etc.)
+2. Add a list of 5-letter words and call it {lang_code}_5words.txt
+    1. (Optional) Add a language_config.json file
+    2. (Optional) Add a keyboard configuration 
+    3. (Optional) Add a list of supplemental words (useful to have this for weird/rare words, and only have 'common' words in the main 5words.txt file)
+
+Voilà!
+
+## How to run locally
+
+If you want to test out your changes, you can run the server locally.
+
+1. Install Python 3
+
+2. Install requirements
+```pip3 install -r requirements.txt```
+
+3. Run web server locally
+```gunicorn --chdir webapp app:app```
+
+4. Navigate to [http://127.0.0.1:8000/](http://127.0.0.1:8000/)
+
+## TODOs
+- [ ] take into account browser bar height on mobile
+- [ ] curate wordlists for existing languages
+- [ ] add keyboard layouts for existing languages
+- [ ] add more languages
+- [ ] fully translate game interface (missing score streaks and options)
+
+Potential improvements:
+- [ ] nice animations for revealing letters & such
+- [ ] make a 4, 6, 7 letter version
+- [ ] deal with accents & character modifiers better (e.g. french is horrible right now)
+
+Nice to haves:
+- [ ] improve code quality (e.g. make variable names consistent, code more reusable & less hacky)
+- [ ] properly integrate TailwindCSS and Vue.js (i.e. not from CDN)
+- [ ] tests...
+
+## Status of Languages
+```  
+  - Hebrew (he):                   🟨 (64540 words)
+  - Serbian (sr):                  🟨 (17968 words)
+  - Central Kurdish (ckb):         🟩 (14819 words)
+  - English (en):                  🟩 (12948 words)
+  - Slovenian (sl):                🟨 (11731 words)
+  - Persian (fa):                  🟨 (11253 words)
+  - Norwegian Nynorsk (nn):        🟨 (10523 words)
+  - Slovak (sk):                   🟨 (10444 words)
+  - Greek (el):                    🟨 (10209 words)
+  - Polish (pl):                   🟨 (10184 words)
+  - Arabic (ar):                   🟨 (10166 words)
+  - Ukrainian (uk):                🟨 (9589 words)
+  - Danish (da):                   🟨 (9516 words)
+  - Estonian (et):                 🟨 (9459 words)
+  - Turkish (tr):                  🟨 (9224 words)
+  - Catalan (ca):                  🟨 (9079 words)
+  - Portuguese (pt):               🟨 (9016 words)
+  - Korean (ko):                   🟨 (8922 words)
+  - Georgian (ka):                 🟨 (8827 words)
+  - Romanian (ro):                 🟨 (8618 words)
+  - Icelandic (is):                🟨 (8285 words)
+  - Czech (cs):                    🟨 (7544 words)
+  - Basque (eu):                   🟨 (7520 words)
+  - Norwegian Bokmål (nb):         🟨 (7471 words)
+  - Dutch (nl):                    🟨 (7441 words)
+  - Breton (br):                   🟨 (7143 words)
+  - Faroese (fo):                  🟨 (6448 words)
+  - Western Frisian (fy):          🟨 (6095 words)
+  - Hungarian (hu):                🟨 (6047 words)
+  - Macedonian (mk):               🟨 (5998 words)
+  - Swedish (sv):                  🟨 (5968 words)
+  - Turkmen (tk):                  🟨 (5814 words)
+  - Latin (la):                    🟨 (5802 words)
+  - Irish (ga):                    🟨 (5082 words)
+  - Bulgarian (bg):                🟨 (4953 words)
+  - Azerbaijan (az):               🟩 (4873 words)
+  - Mongolian (mn):                🟨 (4830 words)
+  - Russian (ru):                  🟨 (4688 words)
+  - Gaelic (gd):                   🟨 (4660 words)
+  - French (fr):                   🟨 (4482 words)
+  - Occitan (oc):                  🟨 (4204 words)
+  - Armenian (hy):                 🟨 (3848 words)
+  - Western Armenian (hyw):        🟨 (3748 words)
+  - Spanish (es):                  🟨 (3602 words)
+  - Croatian (hr):                 🟨 (3591 words)
+  - Fur (fur):                     🟨 (3569 words)
+  - Finnish (fi):                  🟩 (3271 words)
+  - Galician (gl):                 🟨 (3269 words)
+  - Italian (it):                  🟨 (2783 words)
+  - Latvian (lv):                  🟨 (2775 words)
+  - Interlingua (ia):              🟨 (2476 words)
+  - Esperanto (eo):                🟨 (2449 words)
+  - German (de):                   🟨 (2277 words)
+  - Nepali (ne):                   🟨 (2197 words)
+  - Interlingue (ie):              🟨 (2147 words)
+  - Lithuanian (lt):               🟨 (2004 words)
+  - Luxembourgish (lb):            🟨 (1752 words)
+  - Quenya (qya):                  🟨 (1327 words)
+  - Low German (nds):              🟨 (1001 words)
+  - Vietnamese (vi):               🟧 (739 words)
+  - Latgalian (ltg):               🟥 (388 words)
+  - Klingon (tlh):                 🟥 (270 words)
+  - Maori (mi):                    🟥 (128 words)
+  - Kinyarwanda (rw):              🟥 (21 words)
+```
+
+![](scripts/out/n_words.png)
+
+## Credits:
+- Josh Wardle (original Wordle creator)
+- NYT (presumably holds the copyright or some IP rights or something with their $$$ purchase)
+- Elizabeth S (inventor of the Wordle grid)
+- Nadia H (my lovely beta-tester)
+- Daniel Rodriguez (for some inspiration with Tailwind)
+- [Wordles of the World](https://gitlab.com/rwmpelstilzchen/wordles) for a community-sourced list of wordle-derivatives (it's impressive how many actually exist)
+
+## Data sources
+- https://www.nytimes.com/games/wordle/index.html - english word list
+- https://kaino.kotus.fi/sanat/nykysuomi/ - Finnish word list
+- https://github.com/wooorm/dictionaries - most other word lists ([Titus Wormer](https://wooorm.com/) is an incredible dude)
---
+++ webapp/app.py
@@ -0,0 +1,299 @@
+from flask import (
+    Flask,
+    render_template,
+    make_response,
+    redirect,
+    request,
+)
+import json
+
+import datetime
+import glob
+import random
+
+# set random seed 42 for reproducibility (important to maintain stable word lists)
+random.seed(42)
+
+app = Flask(__name__)
+
+###############################################################################
+# DATA
+###############################################################################
+print("Loading data...")
+
+data_dir = "data/"
+# if not glob.glob(data_dir):
+#     data_dir = "../data/"
+# if not glob.glob(data_dir):
+#     data_dir = "webapp/data/"
+# print(f"data_dir: {data_dir}")
+
+
+# load other_wordles.json file
+with open(f"{data_dir}other_wordles.json", "r") as f:
+    other_wordles = json.load(f)
+
+
+def load_characters(lang):
+    if not glob.glob(f"{data_dir}languages/{lang}/{lang}_characters.txt"):
+        characters = set()
+        with open(f"{data_dir}languages/{lang}/{lang}_5words.txt", "r") as f:
+            for line in f:
+                characters.update(line.strip())
+        with open(f"{data_dir}languages/{lang}/{lang}_characters.txt", "w") as f:
+            # sort characters
+            characters = sorted(characters)
+            # write char per newline
+            for char in characters:
+                f.write(char + "\n")
+
+    with open(f"{data_dir}languages/{lang}/{lang}_characters.txt", "r") as f:
+        characters = [line.strip() for line in f]
+    return characters
+
+
+language_codes = [f.split("/")[-1] for f in glob.glob(f"{data_dir}/languages/*")]
+language_characters = {lang: load_characters(lang) for lang in language_codes}
+
+
+def load_words(lang):
+    """loads the words and does some basic QA"""
+    _5words = []
+    with open(f"{data_dir}/languages/{lang}/{lang}_5words.txt", "r") as f:
+        for line in f:
+            _5words.append(line.strip())
+    # QA
+    _5words = [word.lower() for word in _5words if len(word) == 5 and word.isalpha()]
+    # remove words without correct characters
+    _5words = [
+        word
+        for word in _5words
+        if all([char in language_characters[lang] for char in word])
+    ]
+
+    # we don't want words in order, so if .txt is not pre-shuffled, shuffle
+    last_letter = ""
+    n_in_order = 0
+    for word in _5words:
+        letter = word[0]
+        # check if sorted
+        if letter <= last_letter:
+            n_in_order += 1
+        last_letter = letter
+    # if 80% of words are in order, then we consider the list sorted and we shuffle it deterministically
+    if n_in_order / len(_5words) > 0.8:
+        random.shuffle(_5words)
+        print(f"{lang} words are sorted, shuffling")
+    return _5words
+
+
+def load_supplemental_words(lang):
+    """loads the supplemental words file if it exists"""
+    try:
+        with open(f"{data_dir}languages/{lang}/{lang}_5words_supplement.txt", "r") as f:
+            supplemental_words = [line.strip() for line in f]
+        supplemental_words = [
+            word
+            for word in supplemental_words
+            if all([char in language_characters[lang] for char in word])
+        ]
+    except FileNotFoundError:
+        supplemental_words = []
+    return supplemental_words
+
+
+def load_language_config(lang):
+    try:
+        with open(f"{data_dir}languages/{lang}/language_config.json", "r") as f:
+            language_config = json.load(f)
+        return language_config
+    except:
+        # english is fallback (not ideal but better than empty...)
+        with open(f"{data_dir}default_language_config.json", "r") as f:
+            language_config = json.load(f)
+        return language_config
+
+
+def load_keyboard(lang):
+    try:
+        with open(f"{data_dir}languages/{lang}/{lang}_keyboard.json", "r") as f:
+            keyboard = json.load(f)
+        return keyboard
+    except:
+        return []
+
+
+def get_todays_idx():
+    n_days = (datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).days
+    idx = n_days - 18992 + 195
+    return idx
+
+
+language_codes_5words = {l_code: load_words(l_code) for l_code in language_codes}
+language_codes_5words_supplements = {
+    l_code: load_supplemental_words(l_code) for l_code in language_codes
+}
+language_configs = {l_code: load_language_config(l_code) for l_code in language_codes}
+
+keyboards = {k: load_keyboard(k) for k in language_codes}
+
+
+def load_languages():
+    """returns a dict of language codes mapped to their english name and native name"""
+
+    languages = {}
+    # for each language folder, get the language config.name and config.name_natove
+    for lang in language_codes:
+        language_config = language_configs[lang]
+        languages[lang] = {
+            "language_name": language_config["name"],
+            "language_name_native": language_config["name_native"],
+            "language_code": lang,
+        }
+    return languages
+
+
+languages = load_languages()
+
+# status
+with open("../scripts/out/status_list.txt", "r") as f:
+    status_list = [line.strip() for line in f]
+    status_list_str = ""
+    for status in status_list:
+        status_list_str += (
+            f"<option value='{status}'>{status}{'&nbsp;'*(20-len(status))}</option>"
+        )
+    status_list_str += (
+        "<a href='https://github.com/Hugo0/wordle' target='_blank'>more at Github</a>"
+    )
+
+# print stats about how many languages we have
+print("\n***********************************************")
+print(f"                    STATS")
+print(f"- {len(languages)} languages")
+print(
+    f"- {len([k for (k, v) in language_codes_5words_supplements.items() if v !=[]])} languages with supplemental words"
+)
+print(
+    f"- The language with least words is {min(language_codes_5words, key=lambda k: len(language_codes_5words[k]))}, with {len(language_codes_5words[min(language_codes_5words, key=lambda k: len(language_codes_5words[k]))])} words"
+)
+print(
+    f"- The language with most words is {max(language_codes_5words, key=lambda k: len(language_codes_5words[k]))}, with {len(language_codes_5words[max(language_codes_5words, key=lambda k: len(language_codes_5words[k]))])} words"
+)
+print(
+    f"- Average number of words per language is {sum(len(language_codes_5words[l_code]) for l_code in language_codes)/len(language_codes):.2f}"
+)
+print(
+    f"- Average length of supplemental words per language is {sum(len(language_codes_5words_supplements[l_code]) for l_code in language_codes)/len(language_codes):.2f}"
+)
+print(f"- There are {len(other_wordles)} other wordles")
+print(f"***********************************************\n")
+
+
+###############################################################################
+# CLASSES
+###############################################################################
+
+
+class Language:
+    """Holds the attributes of a language"""
+
+    def __init__(self, language_code, word_list):
+        self.language_code = language_code
+        self.word_list = word_list
+        self.word_list_supplement = language_codes_5words_supplements[language_code]
+        todays_idx = get_todays_idx()
+        self.daily_word = word_list[todays_idx % len(word_list)]
+        self.todays_idx = todays_idx
+        self.config = language_configs[language_code]
+
+        self.characters = language_characters[language_code]
+        # remove chars that aren't used to reduce bloat a bit
+        characters_used = []
+        for word in self.word_list:
+            characters_used += list(word)
+        characters_used = list(set(characters_used))
+        self.characters = [char for char in self.characters if char in characters_used]
+
+        self.keyboard = keyboards[language_code]
+        if self.keyboard == []:  # if no keyboard defined, then use available chars
+            # keyboard of ten characters per row
+            for i, c in enumerate(self.characters):
+                if i % 10 == 0:
+                    self.keyboard.append([])
+                self.keyboard[-1].append(c)
+            self.keyboard[-1].insert(0, "⇨")
+            self.keyboard[-1].append("⌫")
+
+            # Deal with bottom row being too crammed:
+            if len(self.keyboard[-1]) == 11:
+                popped_c = self.keyboard[-1].pop(1)
+                self.keyboard[-2].insert(-1, popped_c)
+            if len(self.keyboard[-1]) == 12:
+                popped_c = self.keyboard[-2].pop(0)
+                self.keyboard[-3].insert(-1, popped_c)
+                popped_c = self.keyboard[-1].pop(2)
+                self.keyboard[-2].insert(-1, popped_c)
+                popped_c = self.keyboard[-1].pop(2)
+                self.keyboard[-2].insert(-1, popped_c)
+
+
+###############################################################################
+# ROUTES
+###############################################################################
+
+
+# before request, redirect to https (unless localhost)
+@app.before_request
+def before_request():
+    print("BEFORE REQUEST")
+    if (
+        request.url.startswith("http://")
+        and not "localhost" in request.url
+        and not "127.0.0" in request.url
+    ):
+        url = request.url.replace("http://", "https://", 1)
+        code = 301
+        return redirect(url, code=code)
+
+
+@app.route("/")
+def index():
+    return render_template(
+        "index.html",
+        languages=languages,
+        language_codes=language_codes,
+        todays_idx=get_todays_idx(),
+        other_wordles=other_wordles,
+    )
+
+
+@app.route("/stats")
+def stats():
+    return status_list_str
+
+
+# sitemap
+@app.route("/sitemap.xml")
+def site_map():
+    response = make_response(
+        render_template(
+            "sitemap.xml", languages=languages, base_url="https://wordle.global"
+        )
+    )
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
+
+# arbitrary app route
+@app.route("/<lang_code>")
+def language(lang_code):
+    if lang_code not in language_codes:
+        return "Language not found"
+    word_list = language_codes_5words[lang_code]
+    language = Language(lang_code, word_list)
+    return render_template("game.html", language=language)
+
+
+if __name__ == "__main__":
+    app.run()
